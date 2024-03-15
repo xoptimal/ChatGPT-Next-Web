53dbc40f2acda00b0bccbe8b768b712db3ae827d
Freddy <xoptimal@163.com>
2024-03-15 10:10:25 +0800

    feat 调整样式

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -23,42 +23,36 @@ export default function Guide() {
                     <h1>问答</h1>
                     <h2>QUESTION</h2>
                 </div>
-                <div className={styles.icon_next}><RightCircleFilled/></div>
             </div>
             <div onClick={() => gotoPage('gpt')}>
                 <div className={styles.item}>
                     <h1>指导</h1>
                     <h2>GUIDE</h2>
                 </div>
-                <div className={styles.icon_next}><RightCircleFilled/></div>
             </div>
             <div onClick={() => gotoPage()}>
                 <div className={styles.item}>
                     <h1>规划</h1>
                     <h2>PLAN</h2>
                 </div>
-                <div className={styles.icon_next}><RightCircleFilled/></div>
             </div>
             <div onClick={() => gotoPage()}>
                 <div className={styles.item}>
                     <h1>文书</h1>
                     <h2>BOOK</h2>
                 </div>
-                <div className={styles.icon_next}><RightCircleFilled/></div>
             </div>
             <div onClick={() => gotoPage()}>
                 <div className={styles.item}>
                     <h1>政策</h1>
                     <h2>POLICY</h2>
                 </div>
-                <div className={styles.icon_next}><RightCircleFilled/></div>
             </div>
             <div onClick={() => gotoPage('report')}>
                 <div className={styles.item}>
                     <h1>报告</h1>
                     <h2>REPORT</h2>
                 </div>
-                <div className={styles.icon_next}><RightCircleFilled/></div>
             </div>
         </div>
     </div>
